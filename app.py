490b88e89d0c53195efa4af18661e2f4705607d5
daliluni <sashdash@mail.ru>
2019-11-29 20:14:17 +0300

    json response

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask_api import FlaskAPI
+import json
 
 app = FlaskAPI(__name__)
 
@@ -6,6 +7,14 @@ app = FlaskAPI(__name__)
 @app.route('/')
 def example():
     return 'hello'
+
+
+@app.route('/competition/<int:id>')
+def competition(id):
+    with open('./mock/competition_data.json', 'r') as competition_data_file:
+        competition_data = competition_data_file.read()
+
+    return json.loads(competition_data)
 
 
 if __name__ == '__main__':
