efc42126c47127cdaaa77f58cef6ddb6c87a217a
daliluni <sashdash@mail.ru>
2019-12-07 01:23:38 +0300

    sql retreave

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import jsonify
 from flask_api import FlaskAPI
 import json
+import mysql.connector
+
+
 
 app = FlaskAPI(__name__)
 
-with open('./tmp/competition_data.json', 'r') as competition_data_file:
-    competition_data = competition_data_file.read()
+#with open('./tmp/competition_data.json', 'r') as competition_data_file:
+#    competition_data = competition_data_file.read()
 
 
 @app.route('/')
@@ -14,9 +17,17 @@ def example():
 
 
 @app.route('/competition/<int:id>')
+mydb = mysql.connector.connect(
+  host="tantriks.ru",
+  user="Jora",
+  passwd="daliluni"
+)
+
+mycursor = mydb.cursor()
+
 def competition(id):
     return jsonify(json.loads(competition_data))
 
 
 if __name__ == '__main__':
-    app.run(debug=True, )
+    app.run(debug=True)
